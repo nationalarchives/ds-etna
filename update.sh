803298b9494ba30ad9b08548d1e76c9019d13fec
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-07-30 17:32:52 +0100

    Prune Docker if disk usage is above 80%

--- update.sh
+++ update.sh
@@ -48,4 +48,4 @@ zero_downtime_deploy frontend
 zero_downtime_deploy frontend-develop
 df -h -k /dev/sda1
 # Prune Docker if disk usage is above 80%
-if df -hP -k /dev/sda1 | awk '{if ($5+0<80) exit 1}'; then echo "Space OK"; else docker system prune --force --all --volumes; fi
+if df -hP -k /dev/sda1 | awk '{if ($5+0>=80) exit 1}'; then echo "Space OK"; else docker system prune --force --all --volumes; fi
